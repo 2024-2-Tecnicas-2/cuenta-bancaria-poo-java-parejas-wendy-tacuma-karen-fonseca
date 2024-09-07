a2455eb5db38b69d8034ac2e576fcf3087577cb3
user <user@email.com>
2024-09-07 13:12:20 -0500

    solucion cuenta bancaria

--- src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
@@ -1,5 +1,71 @@
 package com.mycompany.cuenta.bancaria.poo;
 
 public class CuentaBancaria {
+ 
+    private String titular;
+    private String numeroCuenta;
+    private double saldo;
+    private double tipoInteres;
+    
+    //Constructores
+
+    public CuentaBancaria() {
+    }
+
+    public CuentaBancaria(String titular, String numeroCuenta, double saldo) {
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoInteres = 1.5;
+    }
+    
+    // METODOS PIBLICOS
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void ingresar(double cantidad){
+        if(cantidad > 0){
+            saldo += cantidad;
+        } else {
+            System.out.println("La cantidad a ingresar debe ser positiva");
+        }
+    }
+    public void retirar(double cantidad){
+        if(cantidad > 0){
+            if(cantidad <+ saldo){
+                saldo -= cantidad;
+            } else {
+                System.out.println("No se puede retirar mas de lo que hay en la cuenta");
+            }
+        } else {
+            System.out.println("La cantidad a retirar debe ser positiva");
+        }
+    }
+    
+    public double calcularInteres(){
+        return saldo + (saldo * tipoInteres / 100);
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        if(tipoInteres >= 0 && tipoInteres <= 10){
+            this.tipoInteres = tipoInteres;
+        } else {
+            System.out.println("El tipo de interes deber estar entre el 0% y el 10%");
+        }
+    }
+    
     // TODO: Aquí va el código de la clase CuentaBancaria
 }
--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -3,5 +3,39 @@ package com.mycompany.cuenta.bancaria.poo;
 public class Principal {
     public static void main(String[] args) {
         // TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+        CuentaBancaria cuenta = new CuentaBancaria("Wendy Fonseca","5072317169", 1850000.0);
+        
+        System.out.println("Prueba Titular: " + cuenta.getTitular().equals("Wendy Fonseca"));
+        
+        cuenta.setTitular("Karen Tacuma");
+        System.out.println("Prueba Titular: " + cuenta.getTitular().equals("Karen Tacuma"));
+        
+        System.out.println("Prueba Numero de Cuenta: " + cuenta.getNumeroCuenta().equals("5072317196"));
+        
+        System.out.println("Prueba Saldo: "+ (cuenta.getSaldo() == 1780000.0));
+        
+        cuenta.ingresar(335000);
+        System.out.println("Prueba ingresar: " + (cuenta.getSaldo() == 3225000.0));
+        
+        cuenta.ingresar(-200000);
+        System.out.println("Prueba ingresar: " + (cuenta.getSaldo() == 3225000.0));
+        
+        cuenta.retirar(300000);
+        System.out.println("Prueba retirar: " + (cuenta.getSaldo() == 2435000.0));
+        
+        cuenta.retirar(2000000);
+        System.out.println("Prueba retirar: " + (cuenta.getSaldo() == 2435000.0));
+        
+        System.out.println("Calcular interes: " + (cuenta.calcularInteres() == 1876275.0));
+        
+        cuenta.setTipoInteres(5.0);
+        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1432250.0));
+        
+        cuenta.setTipoInteres(-2.0);
+        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1432250.0));
+        
+        cuenta.setTipoInteres(12.0);
+        System.out.println("Prueba interes: " + (cuenta.calcularInteres() == 1432250));
+        
     }
 }
